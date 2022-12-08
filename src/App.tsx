98974e9db973e2b9e95b18038ed6fe3fe4977545
sophiexander <sophie3606@gmail.com>
2022-12-09 11:58:08 +1300

    added icon assets and background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import pilates_logo from "./assets/pilates.png";
+import pilates1_logo from "./assets/pilates_1.png";
+import pilates2_logo from "./assets/pilates_2.png";
+import pilates3_logo from "./assets/pilates_3.png";
+import pilates4_logo from "./assets/pilates_4.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="div-background-img">
+        <img src={pilates2_logo} className="icon" alt="logo" />
+        <img src={pilates1_logo} className="icon" alt="logo" />
+        <img src={pilates_logo} className="icon" alt="logo" />
+
+        <img src={pilates4_logo} className="icon" alt="logo" />
+        <img src={pilates3_logo} className="icon" alt="logo" />
+      </div>
+
+      <footer>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://www.flaticon.com/free-icons/pilates"
+          title="pilates icons"
         >
-          Learn React
+          Pilates icons created by Freepik - Flaticon
         </a>
-      </header>
+        <a
+          href="https://www.flaticon.com/free-icons/exercise"
+          title="exercise icons"
+        >
+          Exercise icons created by Freepik - Flaticon
+        </a>
+        <a
+          href="https://www.flaticon.com/free-icons/meditation"
+          title="meditation icons"
+        >
+          Meditation icons created by Freepik - Flaticon
+        </a>
+      </footer>
     </div>
   );
 }
