1f6982d8735380187042208c6128f03f0bd9e4cd
sophiexander <sophie3606@gmail.com>
2023-04-04 21:48:31 +1200

    Fix import for uikit and use of background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
+import "uikit/dist/css/uikit.css";
 import React from "react";
 import pilates_logo from "./assets/pilates.png";
 import pilates1_logo from "./assets/pilates_1.png";
 import pilates2_logo from "./assets/pilates_2.png";
 import pilates3_logo from "./assets/pilates_3.png";
 import pilates4_logo from "./assets/pilates_4.png";
-import "./App.css";
+import sunset from "./assets/sunset.jpg";
 import Footer from "./Footer";
 import Icon from "./Icon";
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="div-background-img">
-        <Icon image={pilates2_logo}/>
-        <Icon image={pilates1_logo}/>
-        <Icon image={pilates_logo}/>
-        <Icon image={pilates4_logo}/>
-        <Icon image={pilates3_logo}/>
+      <div className="uk-child-width-1@s uk-light" uk-grid>
+        <div
+          className="uk-background-cover uk-height-large uk-panel uk-flex uk-flex-center uk-flex-middle"
+          style={{
+            backgroundImage: `url(${sunset})`,
+          }}
+        >
+          <Icon image={pilates2_logo} />
+          <Icon image={pilates1_logo} />
+          <Icon image={pilates_logo} />
+          <Icon image={pilates4_logo} />
+          <Icon image={pilates3_logo} />
+        </div>
       </div>
-
-      <Footer/>
+      <Footer />
     </div>
   );
 }
