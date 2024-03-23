ad8c54d4834ffc75404ecd54ec6aed66908d516e
sophiexander <sophie3606@gmail.com>
2024-03-24 11:36:36 +1300

    header and logo fixes

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,15 +1,20 @@
 export interface Props {
   image: string;
+  logo?: string;
+  linkName?: string;
+  width?: number;
 }
-export default function Icon({ image }: Props) {
+export default function Icon({ image, logo, linkName, width }: Props) {
   return (
-    <div>
+    <div className="">
       <img
         src={image}
-        className="icon uk-card uk-card-body uk-animation-scale-up"
+        className="icon uk-animation-scale-up"
         alt="logo"
         width={200}
+        style={{ padding: "0 20px" }}
       />
+      <div className="uk-text-center uk-text-bold">{linkName}</div>
     </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import pilates2_logo from "../../assets/pilates_2.png";
 import pilates3_logo from "../../assets/pilates_3.png";
 import pilates4_logo from "../../assets/pilates_4.png";
 import sunset from "../../assets/sunset.jpg";
-import logo from "../../assets/pilateslogo2.png";
+import logo from "../../assets/Pilates_logo.png";
 import Icon from "../Icon";
 
 export default function Header() {
@@ -18,29 +18,29 @@ export default function Header() {
           backgroundImage: `url(${sunset})`,
         }}
       >
-        <div className="uk-flex uk-flex-row uk-flex-center@m">
-          <Icon image={pilates2_logo} />
-          <Icon image={pilates1_logo} />
-          <Icon image={pilates_logo} />
-          <Icon image={pilates4_logo} />
-          <Icon image={pilates3_logo} />
-        </div>
-
-        <div className="heading-gradient uk-flex uk-flex-center">
-          <h1
-            className="uk-heading-large uk-flex uk-flex-bottom"
-            // style={{ background: "purple" }}
-          >
+        <div className="uk-flex uk-flex-row uk-flex-between">
+          <div>
             <img
               src={logo}
               className="uk-animation-scale-up"
               alt="logo"
               width={300}
+              style={{ padding: "10px 0 0 0" }}
             />
+          </div>
 
-            {/* <span style={{ color: "black" }}>Pilates With Antoinette</span> */}
-          </h1>
+          <div
+            className="uk-flex uk-flex-row uk-flex-right@m"
+            style={{ padding: "0 40px 0 0" }}
+          >
+            <Icon image={pilates2_logo} linkName="Home" />
+            <Icon image={pilates1_logo} linkName="About" />
+            <Icon image={pilates_logo} linkName="Timetable" />
+            <Icon image={pilates4_logo} linkName="Prices" />
+            <Icon image={pilates3_logo} linkName="Contact" />
+          </div>
         </div>
+        <div className="heading-gradient uk-flex uk-flex-center"></div>
       </div>
     </div>
   );
